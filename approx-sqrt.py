63bc46cdc7e5136d2a0293f2e99ad6a26c3e00ea
Richard Cooke <g00331787@gmit.ie>
2019-10-30 17:51:57 +0000

    First Commit

--- approx-sqrt.py
+++ approx-sqrt.py
@@ -3,10 +3,14 @@ def sqrt(x):
     z = 1.0
 
 #Keep getting a better estiate for the square root until you are within 2 decimal places
-    while abs(z*z- x) >= 0.01
+    while abs(z*z- x) >= 0.0000001:
         # Get a better approx for the square root
         z -= (z*z - x) / (2*z)
 
     return z
 
-sqrt(8.0)
+z = sqrt(64.0)
+
+print(z)
+
+print(z*z)
